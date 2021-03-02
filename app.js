98434c47622f0926ca68cb136c373715763a88d3
Rishab Agarwala <ragarwala2097@gmail.com>
2021-03-02 17:36:33 +0530

    second push

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const api = require("./api");
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const {urls} = require("./urlData");
 
 app.use(express.json());
